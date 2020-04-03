18360c601f33e79f5b9a793c8fae268ed2169a06
monil <monil.panchal@dal.ca>
2020-04-02 23:17:28 -0300

    [fix] error handling in location based service

--- app/src/main/java/com/book/chore/ui/home/home/HomeFragment.kt
+++ app/src/main/java/com/book/chore/ui/home/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.book.chore.ui.login.home.ui.home
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -24,13 +25,13 @@ import com.book.chore.ui.location.LocationFragment
 import com.book.chore.ui.services.ServicesActivity
 import com.book.chore.utils.ChoreConstants
 import com.bumptech.glide.Glide
-import java.lang.Exception
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: HomeFragmentBinding
     private lateinit var viewModel: SharedViewModel
-    private var city = ""
+    private var city = "default"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,7 +61,7 @@ class HomeFragment : Fragment() {
                 override fun onChanged(t: MutableMap<String, Any>?) {
                     if (t != null) {
                         city = t.get(ChoreConstants.AppConstant.SERVICE_CITY) as String
-                        Log.i("Location changed: ",city)
+                        Log.i("Location changed: ", city)
                         fetchServices()
                     }
                 }
@@ -99,19 +100,21 @@ class HomeFragment : Fragment() {
     }
 
     private fun fetchServices() {
-
-        if (city == "") {
+        if (city == "default") {
             ServiceManager().fetchAvailableServices {
                 Log.i("service list", it.toString())
                 val adapter = ChoreServicesAdapter(it, object : OnItemClickListener {
                     override fun onItemClick(item: ChoreService?) {
-                        if (item != null) {
-                            Toast.makeText(context, item.serviceName, Toast.LENGTH_LONG).show()
-                            //Add code to filter the task doers
-                            val i = Intent(context, ServicesActivity::class.java)
-                            i.putExtra(ChoreConstants.AppConstant.SERVICE_CITY, city)
-                            startActivity(i)
+
+                        val builder = AlertDialog.Builder(this@HomeFragment.requireContext())
+                        builder.setTitle(ChoreConstants.AlertConstant.SERVICE_SELECT_LOCATION_TITLE)
+                        builder.setMessage(ChoreConstants.AlertConstant.SERVICE_SELECT_LOCATION_MESSAGE)
+                        builder.setPositiveButton(ChoreConstants.AlertConstant.OKAY_BUTTON) { dialog, which ->
                         }
+                        val alert = builder.create()
+                        alert.setTitle(ChoreConstants.AlertConstant.SERVICE_SELECT_LOCATION_TITLE)
+                        alert.show()
+
                     }
                 })
                 binding.servicesList.layoutManager = GridLayoutManager(context, 2)
@@ -119,27 +122,54 @@ class HomeFragment : Fragment() {
             }
         } else {
             Log.i("Fetching services for: ", city)
-            ServiceManager().fetchAvailableServicesWithCity(city) {
-                Log.i("service list with city", it.toString())
+//          ServiceManager().fetchAvailableServicesWithCity(city) {
+//                Log.i("service list with city", it.toString())
+//
+//                if(it.isEmpty()){
+//                    Toast.makeText(getContext(),"Services not found for $city",Toast.LENGTH_SHORT).show()
+//                }
+//
+//                val adapter = ChoreServicesAdapter(it, object : OnItemClickListener {
+//                    override fun onItemClick(item: ChoreService?) {
+//                        if (item != null) {
+//                            Toast.makeText(context, item.serviceName, Toast.LENGTH_LONG).show()
+//                            //Add code to filter the task doers
+//                            val i = Intent(context, ServicesActivity::class.java)
+//                            i.putExtra(ChoreConstants.AppConstant.SERVICE_CITY, city)
+//                            startActivity(i)
+//                        }
+//                    }
+//                })
+//                binding.servicesList.layoutManager = GridLayoutManager(context, 2)
+//                binding.servicesList.adapter = adapter
+//            }
 
-                if(it.isEmpty()){
-                    Toast.makeText(getContext(),"Services not found for $city",Toast.LENGTH_SHORT).show()
-                }
 
-                val adapter = ChoreServicesAdapter(it, object : OnItemClickListener {
-                    override fun onItemClick(item: ChoreService?) {
-                        if (item != null) {
-                            Toast.makeText(context, item.serviceName, Toast.LENGTH_LONG).show()
-                            //Add code to filter the task doers
-                            val i = Intent(context, ServicesActivity::class.java)
-                            i.putExtra(ChoreConstants.AppConstant.SERVICE_CITY, city)
-                            startActivity(i)
-                        }
+            var servicesList = ServiceManager().fetchAvailableServicesWithCity(city) {}
+            val adapter = ChoreServicesAdapter(servicesList, object : OnItemClickListener {
+                override fun onItemClick(item: ChoreService?) {
+
+                    if (servicesList.isEmpty()) {
+                        Toast.makeText(
+                            getContext(),
+                            "Services not found for $city",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
-                })
-                binding.servicesList.layoutManager = GridLayoutManager(context, 2)
-                binding.servicesList.adapter = adapter
-            }
+
+                    if (item != null) {
+                        Toast.makeText(context, item.serviceName, Toast.LENGTH_LONG).show()
+                        //Add code to filter the task doers
+                        val i = Intent(context, ServicesActivity::class.java)
+                        i.putExtra(ChoreConstants.AppConstant.SERVICE_CITY, city)
+                        i.putExtra("serviceType",item.serviceName)
+                        startActivity(i)
+                    }
+                }
+            })
+            binding.servicesList.layoutManager = GridLayoutManager(context, 2)
+            binding.servicesList.adapter = adapter
+
         }
     }
 }
